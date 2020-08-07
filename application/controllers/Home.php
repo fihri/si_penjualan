81efeeec1c5d47237cf52d47db99caa9386af005
Fihri Aziz <fihriaziz@gmail.com>
2020-08-07 10:24:33 +0700

    modifikasi fitur dan tambah baru

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -18,6 +18,45 @@ class Home extends CI_Controller {
 		$this->load->view('template/script');
 	}
 
+    public function widgets() 
+    {
+        $data['title'] = 'Widget Penjualan';
+        $this->load->view('template/header', $data);
+        $this->load->view('template/navbar');
+        $this->load->view('template/sidebar');
+        $this->load->view('widgets');
+        $this->load->view('template/script');
+    }
+
+    public function charts()  
+    {
+        $data['title'] = 'Charts Penjualan';
+        $this->load->view('template/header', $data);
+        $this->load->view('template/navbar');
+        $this->load->view('template/sidebar');
+        $this->load->view('charts');
+        $this->load->view('template/script');
+    }
+
+    public function elements()
+    {
+        $data['title'] = 'UI Element Penjualan';
+        $this->load->view('template/header', $data);
+        $this->load->view('template/navbar');
+        $this->load->view('template/sidebar');
+        $this->load->view('elements');
+        $this->load->view('template/script');
+    }
+
+    public function panel()
+    {
+        $data['title'] = 'Alert & Panel Penjualan';
+        $this->load->view('template/header', $data);
+        $this->load->view('template/navbar');
+        $this->load->view('template/sidebar');
+        $this->load->view('panels');
+        $this->load->view('template/script');
+    }
 }
 
 /* End of file  */
